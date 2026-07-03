83d0bc6ac0dfec58a446c26c73945decf16b1f89
Navyasripuvvada <puvvadanavyasri5809@gmail.com>
2026-07-03 19:16:38 +0530

    backend url update

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: `${process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ?? "http://localhost:5000"}/:path*`,
+        destination: `${process.env.NEXT_PUBLIC_API_URL?.replace(/\/$/, "") ?? "https://globalco-assessment-backend-eight.vercel.app/"}/:path*`,
       },
     ];
   },
